e8a041a22c02c23ff6994d081f13a3d827fcdaa9
diegorafaelgit <diegorafael19977@gmail.com>
2024-11-06 19:37:30 -0300

    consulta da api funcionando

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost:3306/my_bd'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost:3306/cadastroprodutos'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 class Produto(db.Model):
-    __tablename__ = 'produtos'
+    __tablename__ = 'produto'
     id_serial = db.Column(db.Integer, primary_key=True)
     descricao = db.Column(db.String(100), nullable=False)
     custo = db.Column(db.Float, nullable=False)
@@ -18,14 +18,12 @@ class Produto(db.Model):
         return {"id_serial": self.id_serial, "descricao": self.descricao, "custo": self.custo}
 
 class Loja(db.Model):
-    __tablename__ = 'lojas'
+    __tablename__ = 'loja'
     id_serial = db.Column(db.Integer, primary_key=True)
     descricao = db.Column(db.String(100), nullable=False)
     
     def to_dict(self):
         return {"id_serial": self.id_serial, "descricao": self.descricao}
-
-db = SQLAlchemy(app)
 
 @app.route('/produtos', methods=['GET'])
 def get_produtos():
@@ -37,21 +35,5 @@ def get_lojas():
     lojas = Loja.query.all()
     return jsonify([loja.to_dict() for loja in lojas])
 
-# @app.route('/produtos', methods=['GET'])
-# def get_produtos():
-#     return jsonify(produtos) 
-
-# @app.route('/lojas', methods=['GET'])
-# def get_lojas():
-#     return jsonify(lojas) 
-
-# @app.route('/produtos', methods=['POST'])
-# def create_produto():
-#     produto = request.json
-#     produtos.append(produto)
-#     return produto
-
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
